f23060b6540534ead702eb1518eae32ad96d9ae5
CCarballo <CCarballo@192.168.1.105>
2017-11-30 16:55:56 -0700

    Correcion de la entidad Curso y eliminacion de entidad erronea CursoUsuario.

--- Schoology/mx/itson/schoology/entidades/Curso.java
+++ Schoology/mx/itson/schoology/entidades/Curso.java
@@ -1,54 +1,71 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
+import java.util.List;
 
 /**
  *
- * @author Francisco
+ * @author CCarballo
  */
 public class Curso{
     
     private int id;
     private String codigo;
     private String nombre;
-    private int idUsuario;
+    private List<Usuario> miembros;
 
+    /**
+     * @return the id
+     */
     public int getId() {
         return id;
     }
 
+    /**
+     * @param id the id to set
+     */
     public void setId(int id) {
         this.id = id;
     }
 
+    /**
+     * @return the codigo
+     */
     public String getCodigo() {
         return codigo;
     }
 
+    /**
+     * @param codigo the codigo to set
+     */
     public void setCodigo(String codigo) {
         this.codigo = codigo;
     }
 
+    /**
+     * @return the nombre
+     */
     public String getNombre() {
         return nombre;
     }
 
+    /**
+     * @param nombre the nombre to set
+     */
     public void setNombre(String nombre) {
         this.nombre = nombre;
     }
 
-    public int getIdUsuario() {
-        return idUsuario;
+    /**
+     * @return the miembros
+     */
+    public List<Usuario> getMiembros() {
+        return miembros;
     }
 
-    public void setIdUsuario(int idUsuario) {
-        this.idUsuario = idUsuario;
+    /**
+     * @param miembros the miembros to set
+     */
+    public void setMiembros(List<Usuario> miembros) {
+        this.miembros = miembros;
     }
-    
-    
-    
-    
-    
+        
 }
