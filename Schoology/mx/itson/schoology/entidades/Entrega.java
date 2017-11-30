f32916574b0fdfc316d107e9ec9f8ed0fea25271
Dell <Dell@10.232.6.59>
2017-11-30 10:20:01 -0700

    Creacion de entidad Entrega

---
+++ Schoology/mx/itson/schoology/entidades/Entrega.java
@@ -0,0 +1,72 @@
+
+import java.io.File;
+import java.util.Date;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author JoseLuisChito
+ */
+public class Entrega {
+    private int id;
+    private File archivoAdjunto;
+    private String descripcion;
+    private Date fecha;
+    private int idUsuario;
+    private int idActividad;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public File getArchivoAdjunto() {
+        return archivoAdjunto;
+    }
+
+    public void setArchivoAdjunto(File archivoAdjunto) {
+        this.archivoAdjunto = archivoAdjunto;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public Date getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(Date fecha) {
+        this.fecha = fecha;
+    }
+
+    public int getIdUsuario() {
+        return idUsuario;
+    }
+
+    public void setIdUsuario(int idUsuario) {
+        this.idUsuario = idUsuario;
+    }
+
+    public int getIdActividad() {
+        return idActividad;
+    }
+
+    public void setIdActividad(int idActividad) {
+        this.idActividad = idActividad;
+    }
+    
+    
+}
