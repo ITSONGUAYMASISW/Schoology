bb8aea588c66cf9c72fe99ae60e3e3c7a21a3458
Francisco <Francisco@LAPTOP-QKMKPDVB>
2017-11-30 10:08:34 -0700

    Se agrego clase CursoUsuario

---
+++ Schoology/mx/itson/schoology/entidades/CursoUsuario.java
@@ -0,0 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Francisco
+ */
+public class CursoUsuario {
+    
+    private int id;
+    private int idCurso;
+    private int idUsuario;
+    private double calificacionFinal;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getIdCurso() {
+        return idCurso;
+    }
+
+    public void setIdCurso(int idCurso) {
+        this.idCurso = idCurso;
+    }
+
+    public int getIdUsuario() {
+        return idUsuario;
+    }
+
+    public void setIdUsuario(int idUsuario) {
+        this.idUsuario = idUsuario;
+    }
+
+    public double getCalificacionFinal() {
+        return calificacionFinal;
+    }
+
+    public void setCalificacionFinal(double calificacionFinal) {
+        this.calificacionFinal = calificacionFinal;
+    }
+    
+    
+    
+}
